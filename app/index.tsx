5f7538068b9d6a8316e57b6c87430dd7ab9b5749
seyamasan <kst_apr_24@icloud.com>
2025-09-06 13:51:18 +0900

    feat: ボタンのタップでインクリメントされるように実装

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,18 @@
+import { useState } from 'react';
 import { Text, View, Button, Alert } from "react-native";
 
 export default function Index() {
+  const [count, setCount] = useState(0); // フックを用いて状態管理
+
+  const handleButtonPress = () => {
+    setCount(count + 1) // 状態を更新
+    
+    Alert.alert(
+      'ボタンをタップしました。',
+      `+1されます。`
+    ) // このアラートはモバイルのみで表示されるみたい
+  };
+
   return (
     <View
       style={{
@@ -9,10 +21,12 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>ボタンです。</Text>
+      <Text>{ count }回ボタンをタップしました。</Text>
       <Button
-        onPress={() => Alert.alert('Simple Button pressed')} // このアラートはモバイルのみで表示されるみたい
-        title="Learn More"
+        onPress={
+          () => handleButtonPress()
+        } 
+        title="カウントアップ"
         color="#841584"
       />
     </View>
