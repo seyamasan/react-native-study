9b04b9f8c726b557ba3c07bc5514a4696577d6bc
seyamasan <kst_apr_24@icloud.com>
2025-10-30 19:39:16 +0900

    fix: draggable-flatlistの画面に遷移できるように修正

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,14 @@
 import { Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: 'サンプル集' }} />
-      <Stack.Screen name="counter/index" options={{ title: 'カウンター' }} />
-    </Stack>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Stack>
+        <Stack.Screen name="index" options={{ title: 'サンプル集' }} />
+        <Stack.Screen name="counter/index" options={{ title: 'カウンター' }} />
+        <Stack.Screen name="draggable-flatlist-sample/index" options={{ title: 'draggable-flatlistのサンプル' }} />
+      </Stack>
+    </GestureHandlerRootView>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -11,6 +11,10 @@ export default function Index() {
         <Link href="/counter" asChild>
           <Button title="カウンター画面へGo" color={colors.primary} />
         </Link>
+        <View style={styles.space} />
+        <Link href="/draggable-flatlist-sample" asChild>
+          <Button title="draggable-flatlistのサンプル画面へGo" color={colors.primary} />
+        </Link>
       </ScrollView>
     </View>
   );
