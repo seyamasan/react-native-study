485db0769c6d8cc409ac5af412d663d11be30748
seyamasan <kst_apr_24@icloud.com>
2025-11-03 22:56:49 +0900

    fix: フッターが見えずらいので修正

--- app/screen/DraggableFlatListSample.tsx
+++ app/screen/DraggableFlatListSample.tsx
@@ -141,7 +141,7 @@ const styles = StyleSheet.create({
   },
   footer: {
     paddingHorizontal: 16,
-    paddingVertical: 12,
+    paddingVertical: 24,
     borderTopWidth: StyleSheet.hairlineWidth,
     borderTopColor: '#E0E0E0',
     backgroundColor: '#FFFFFF',
